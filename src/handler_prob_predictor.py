0ee3d0909f1b803b49badf5d0793c30078b8ab56
Nixam01 <marlatawiec01@gmail.com>
2023-01-21 18:45:11 +0100

    handlers

---
+++ src/handler_prob_predictor.py
@@ -0,0 +1,52 @@
+'''
+*===========================================================================*
+*				Author: Marcin Latawiec                                    *
+*===========================================================================*
+'''
+
+''' class for handling probabilistic approach'''
+class ProbabilisticPredictor:
+    def __int__(self, unique_names_probability_dict, tree):
+        self.probability_dict = unique_names_probability_dict
+        self.tree = tree
+
+    def fill_prob_dictionary(self, x, tree, probability = 1):
+        ''' recursive function to fill probability_dictionary '''
+        '''
+        :param x: 
+        :param tree: 
+        :param probability: 
+        :return dictionary: 
+        '''
+        if tree.value!=None:
+            self.probability_dict[tree.value] = self.probability_dict[tree.value] + probability
+
+        else:
+            feature_val = x[tree.feature_index]
+            if not self.is_missing(feature_val):
+                if feature_val<=tree.threshold:
+                    return self.fill_prob_dictionary(x, tree.left, probability)
+                else:
+                    return self.fill_prob_dictionary(x, tree.right, probability)
+            else:
+                number_of_elements_left = len(tree.left_dataset)
+                number_of_elements_right = len(tree.right_dataset)
+                sum_of_elements = number_of_elements_right + number_of_elements_left
+                probability_of_left_branch = probability*number_of_elements_left/(sum_of_elements)
+                probability_of_right_branch = probability*number_of_elements_right/(sum_of_elements)
+                self.fill_prob_dictionary(x, tree.left, probability_of_left_branch)
+                self.fill_prob_dictionary(x, tree.left, probability_of_right_branch)
+
+    def most_possible_value(self):
+        '''function to find most fitting value for given element'''
+        most_possible = list(self.probability_dict.keys())[0]
+        for key, value in self.probability_dict.items():
+            if value>self.probability_dict[most_possible]:
+                most_possible = key
+        return most_possible
+
+    def is_missing(self, value):
+        if str(value) == "NaN" or str(value) == "nan":
+            return True
+        else:
+            return False
--- src/readme.md
+++ src/readme.md
@@ -5,3 +5,7 @@ Sekcja notatki i komentarze, (do usunięcia)
 - Algorytm drzewa decyzyjnego
 - Należy uodpornić klasyfikator na brakujące wartości atrybutów podczas testowania.
 - Należy zaimplementować i przebadać co najmniej 2 metody uodparniające
+
+
+Performance Metrics Tutorial:
+https://towardsdatascience.com/performance-metrics-in-machine-learning-part-1-classification-6c6b8d8a8c92
