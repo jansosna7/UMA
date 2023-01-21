6b4dc3a3f5821706b9f48033d5baf05d9370a292
Nixam01 <marlatawiec01@gmail.com>
2023-01-21 22:11:45 +0100

    another handler - filler

--- src/handler_filler.py
+++ src/handler_filler.py
@@ -3,5 +3,38 @@
 *				Author: Marcin Latawiec                                    *
 *===========================================================================*
 '''
+from scipy.stats import rice
 
 ''' class for handling missing values by filling it'''
+
+from enum import Enum, auto
+import numpy as np
+
+class FillDataMethod(Enum):
+    MEAN = auto()
+    MEDIAN = auto()
+    MODE = auto()
+class Filler:
+    def __int__(self, unique_names_probability_dict, tree):
+        self.probability_dict = unique_names_probability_dict
+        self.tree = tree
+
+    def fill_missing_data(self, dataset, method):
+        rows, cols = dataset.shape
+        for col_index in range(cols):
+            column = dataset[:, col_index]
+            replacement = self.get_replacement_value(column, method)
+            column[np. isnan(column)] = replacement
+
+
+    def get_replacement_value(self, column, method):
+        if method is FillDataMethod.MEAN:
+            return np.nanmean(column)
+        if method is FillDataMethod.MEDIAN:
+            return np.nanmedian(column)
+        if method is FillDataMethod.MODE:
+            unique, counts = np.unique(column, return_counts=True)
+            values_frequency = dict(zip(unique, counts))
+            return max(values_frequency, key=values_frequency.get)
+        raise TypeError('Unsupported method')
+
